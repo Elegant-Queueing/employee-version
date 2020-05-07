f4271a64eb0aec5d6dc5f2cecc72f8346ced6ee4
Alex Zhang <zhangbor@cs.washington.edu>
2020-05-06 17:23:17 -0700

    Employee Profile, Queue Info, and Student Info page

--- index.js
+++ index.js
@@ -1,23 +1,86 @@
-// Transition from 1st page to 2nd page
+// Transition from login page to list_of_fairs page
 document.getElementById("login").onclick = function() {
-    document.getElementById("first_page").style.display = "none";
-    document.getElementById("second_page").style.display = "block";
+    document.getElementById("login_page").style.display = "none";
+    document.getElementById("list_of_fairs_page").style.display = "block";
 }
 
-// Transition from 1st page to 3rd page
+// Transition from list_of_fairs page to fair_info page
 document.getElementById("select_fair").onclick = function() {
-    document.getElementById("second_page").style.display = "none";
-    document.getElementById("third_page").style.display = "block";
+    document.getElementById("list_of_fairs_page").style.display = "none";
+    document.getElementById("fair_info_page").style.display = "block";
 }
 
-// Back to 1st page
-document.getElementById("back_1").onclick = function() {
-    document.getElementById("first_page").style.display = "block";
-    document.getElementById("second_page").style.display = "none";
+// Transition from list_of_fairs page to employee profile page
+document.getElementById("employee_profile_button").onclick = function() {
+    document.getElementById("list_of_fairs_page").style.display = "none";
+    document.getElementById("employee_profile_page").style.display = "block";
 }
 
-// Back to 2nd page
-document.getElementById("back_2").onclick = function() {
-    document.getElementById("second_page").style.display = "block";
-    document.getElementById("third_page").style.display = "none";
+// Transition from fair_info page to queue info page
+document.getElementById("add_queue").onclick = function() {
+    document.getElementById("fair_info_page").style.display = "none";
+    document.getElementById("queue_page").style.display = "block";
+}
+
+// Transition from queue info page to student profile page
+document.getElementById("student_profile_1").onclick = function() {
+    document.getElementById("queue_page").style.display = "none";
+    document.getElementById("student_profile_page").style.display = "block";
+}
+
+// Back to login page
+document.getElementById("back_to_login").onclick = function() {
+    document.getElementById("login_page").style.display = "block";
+    document.getElementById("list_of_fairs_page").style.display = "none";
+}
+
+// Back to list of fairs page
+document.getElementById("back_to_fairs").onclick = function() {
+    document.getElementById("list_of_fairs_page").style.display = "block";
+    document.getElementById("fair_info_page").style.display = "none";
+}
+
+// Back to list of fairs page
+document.getElementById("back_to_fairs_e").onclick = function() {
+    document.getElementById("editing_profile").style.display = "none";
+    document.getElementById("employee_profile").style.display = "block";
+    document.getElementById("list_of_fairs_page").style.display = "block";
+    document.getElementById("employee_profile_page").style.display = "none";
+}
+
+// Back to fair_info page
+document.getElementById("stop_queue").onclick = function() {
+    document.getElementById("fair_info_page").style.display = "block";
+    document.getElementById("queue_page").style.display = "none";
+}
+
+// Back to queue info page
+document.getElementById("back_to_queue").onclick = function() {
+    document.getElementById("queue_page").style.display = "block";
+    document.getElementById("student_profile_page").style.display = "none";
+    // TODO: Save tags
+}
+
+
+// Edit profile
+document.getElementById("edit_employee_profile").onclick = function() {
+    document.getElementById("editing_profile").style.display = "block";
+    document.getElementById("employee_profile").style.display = "none";
+}
+// Submit profile changes
+document.getElementById("submit-employee-profile").onclick = function() {
+    document.getElementById("editing_profile").style.display = "none";
+    document.getElementById("employee_profile").style.display = "block";
+}
+
+
+// Remove student
+document.getElementById("skip_student_1").onclick = function() {
+    document.getElementById("student_1").style.display = "none";
+}
+
+// Remove student
+document.getElementById("remove_student_1").onclick = function() {
+    document.getElementById("student_1").style.display = "none";
+    // TODO: Save tags
 }
