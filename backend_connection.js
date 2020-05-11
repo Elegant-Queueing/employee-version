e93a19b2359cab9352b8af4e95230f158cc31e0d
Alex Zhang <zhangbor@cs.washington.edu>
2020-05-11 02:24:57 -0700

    Finish Mock APIs on all pages

--- backend_connection.js
+++ backend_connection.js
@@ -12,15 +12,110 @@ class Connection {
              "start_time":124521515,
              "end_time":124124667,
              "university":"University of Washington",
-             "desc":"Hosted by UW CSE"
+             "desc":"Hosted by UW CSE",
+             "fairId":"abc1"
             },
             {"name":"UW Established Company Fair",
              "location":"HUB South Ballroom",
              "start_time":124521515,
              "end_time":124124667,
              "university":"University of Washington",
-             "desc":"Hosted by UW CSE Established Companies"
+             "desc":"Hosted by UW CSE Established Companies",
+             "fairId":"abc123"
             }
         ]
+    }
+
+    addMyQueue(employeeId, fairId) {
+        return { "created":true };
+    }
+
+    getEmployeeProfile(employeeId) {
+        return {
+            "name":"Alex",
+            "company":"Amazon",
+            "position":"SDE 1",
+            "bio":"Hello World",
+            "img":"profile.png",
+            "email":"abc123@gmail.com"
+       }
+    }
+
+    updateEmployeeProfile(employeeId, name, email, bio) {
+        return { "updated":true };
+    }
+
+    getPastFairsForEmployee(employeeId) {
+        return [
+            {"name":"UW CSE Fair",
+             "location":"HUB South Ballroom",
+             "start_time":124521515,
+             "end_time":124124667,
+             "university":"University of Washington",
+             "desc":"Hosted by UW CSE",
+             "fairId":"abc1"
+            },
+            {"name":"UW Established Company Fair",
+             "location":"HUB South Ballroom",
+             "start_time":124521515,
+             "end_time":124124667,
+             "university":"University of Washington",
+             "desc":"Hosted by UW CSE Established Companies",
+             "fairId":"abc123"
+            }
+        ]
+    }
+
+    getPastStudentsForEmployee(employeeId, fairId) {
+        return [
+            {"name":"Alex Zhang",
+             "studentId":"student1",
+             "tags":["Excellent", "Yes"]
+            },
+            {"name":"Alex Z",
+             "studentId":"student2",
+             "tags":["Nope", "Yes"]
+            }
+        ]
+    }
+
+    getStudentProfile(studentId) {
+        return {
+            "name":"Alex Zhang",
+            "img":"profile.png",
+            "university":"University of Washington",
+            "major":"CSE",
+            "graduationDate":20551151515,
+            "resume":"Resume"
+       }
+    }
+
+    getMyStudents(employeeId) {
+        return [
+            {
+                "name":"Alex Zhang",
+                "studentId":"student1"
+            },
+            {
+                "name":"Alex Z",
+                "studentId":"student2"
+            }
+        ]
+    }
+
+    skipStudent(studentId, employeeId) {
+        return { "skipped":true }
+    }
+
+    registerStudent(employeeId, studentId, fairId, tags) {
+        return { "registered":true }
+    }
+
+    getTags(employeeId) {
+        return ["Excellent", "Yes", "Cool"];
+    }
+
+    stopMyQueue(employeeId) {
+        return { "stopped":true }
     }
 }
--- index.js
+++ index.js
@@ -2,13 +2,23 @@ var connect = new Connection();
 var employeeId = "";
 var listOfFairs = [];
 var fairs;
+var fairId;
+var employeeInfo;
+var listOfFairsHistory = [];
+var listOfStudentsHistory = [];
+var listOfTags = connect.getTags(employeeId);
+
+var queue = [];
+var notOperatingQueue = true;
+var queueStopped = false;
+var update;
 
 // Transition from login page to list_of_fairs page
 document.getElementById("login").onclick = function() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
 
-    var result = connect.login(username, password)
+    var result = connect.login(username, password);
     if (result["authenticated"]) {
         document.getElementById("login_page").style.display = "none";
         document.getElementById("list_of_fairs_page").style.display = "block";
@@ -53,25 +63,308 @@ function showFairInfo(fairName) {
         "<div>Start Time: " + startTime + "</div>" +
         "<div>End Time: " + endTime + "</div>" +
         "<div>Description: " + fair["desc"] + "</div>";
+    fairId = fair["fairId"];
 }
 
 // Transition from list_of_fairs page to employee profile page
 document.getElementById("employee_profile_button").onclick = function() {
     document.getElementById("list_of_fairs_page").style.display = "none";
     document.getElementById("employee_profile_page").style.display = "block";
+
+    employeeInfo = connect.getEmployeeProfile(employeeId);
+
+    document.getElementById("employee_info").innerHTML = 
+        "<div>Image: " + employeeInfo["img"] + "</div>" +
+        "<div>Name: " + employeeInfo["name"] + "</div>" +
+        "<div>Company: " + employeeInfo["company"] + "</div>" +
+        "<div>Position: " + employeeInfo["position"] + "</div>" +
+        "<div>Email: " + employeeInfo["email"] + "</div>" +
+        "<div>Bio: " + employeeInfo["bio"] + "</div>";
 }
 
 // Transition from fair_info page to queue info page
 document.getElementById("add_queue").onclick = function() {
-    document.getElementById("fair_info_page").style.display = "none";
-    document.getElementById("queue_page").style.display = "block";
+    var result = connect.addMyQueue(employeeId, fairId);
+
+    if (result["created"]) {
+        document.getElementById("fair_info_page").style.display = "none";
+        document.getElementById("queue_page").style.display = "block";
+    }
+
+    updateQueue();
+    update = setInterval(updateQueue, 5000);
+}
+
+function updateQueue() {
+    if (notOperatingQueue) {
+        notOperatingQueue = false;
+        queue = connect.getMyStudents(employeeId);
+
+        var inner = "";
+        if (queue.length > 0) {
+            var student = queue[0];
+            inner +=
+                "<div> Student: " +  
+                    "<div style=\"display: inline-block;\">" + student["name"] + "</div>" +             
+                    "<button style=\"display: inline-block;\" onclick=\"showStudentProfile('" + student["studentId"]+ "');\">Profile</button>" +
+                    "<button style=\"display: inline-block;\" onclick=\"skipStudent('" + student["studentId"]+ "');\">Skip</button>" +
+                    "<button style=\"display: inline-block;\" onclick=\"doneTalkingStudent('" + student["studentId"]+ "');\">Done Talking</button>" +
+                "</div>";
+        }
+
+        for (i = 1; i < queue.length; i++) {
+            inner += 
+                "<div> Student: " +  
+                    "<div style=\"display: inline-block;\">" + student["name"] + "</div>" +
+                "</div>";
+        }
+
+        document.getElementById("queue").innerHTML = inner;
+        notOperatingQueue = true;
+    }
 }
 
 // Transition from queue info page to student profile page
-document.getElementById("student_profile_1").onclick = function() {
+function showStudentProfile(studentId) {
     document.getElementById("queue_page").style.display = "none";
     document.getElementById("student_profile_page").style.display = "block";
+
+    var student = connect.getStudentProfile(studentId);
+    document.getElementById("student_info").innerHTML = 
+        "<div> Image: " + student["img"] + "</div>" +
+        "<div> Name: " + student["name"] + "</div>" +
+        "<div> University: " + student["university"] + "</div>" +
+        "<div> Major: " + student["major"] + "</div>" +
+        "<div> Graduation Date: " + new Date(student["graduationDate"]) + "</div>" +
+        "<div> Resume: " + student["resume"] + "</div>";
 }
+// Create tags
+var innerHTMLTags = "";
+for (i = 0; i < listOfTags.length; i++) {
+    innerHTMLTags +=
+        "<div>" + listOfTags[i] + "<input type=\"checkbox\" value=true id=\"tag_" + i + "\"></div>";
+}
+document.getElementById("list_of_tags").innerHTML = innerHTMLTags;
+
+// Skip student
+function skipStudent(studentId) {
+    if (notOperatingQueue) {
+        notOperatingQueue = false;
+        var result = connect.skipStudent(studentId, employeeId);
+
+        if (result["skipped"]) {
+            queue.shift();
+
+            var inner = "";
+            if (queue.length > 0) {
+                var student = queue[0];
+                inner +=
+                    "<div> Student: " +  
+                        "<div style=\"display: inline-block;\">" + student["name"] + "</div>" +             
+                        "<button style=\"display: inline-block;\" onclick=\"showStudentProfile('" + student["studentId"]+ "');\">Profile</button>" +
+                        "<button style=\"display: inline-block;\" onclick=\"skipStudent('" + student["studentId"]+ "');\">Skip</button>" +
+                        "<button style=\"display: inline-block;\" onclick=\"doneTalkingStudent('" + student["studentId"]+ "');\">Done Talking</button>" +
+                    "</div>";
+            }
+
+            for (i = 1; i < queue.length; i++) {
+                inner += 
+                    "<div> Student: " +  
+                        "<div style=\"display: inline-block;\">" + student["name"] + "</div>" +
+                    "</div>";
+            }
+
+            document.getElementById("queue").innerHTML = inner; 
+        }
+        notOperatingQueue = true;
+    } else {
+        alert("Operation happened on current queue. Try again.");
+    }
+}
+
+// Done talking with a student
+function doneTalkingStudent(studentId) {
+    if (notOperatingQueue) {
+        notOperatingQueue = false;
+        var tags = [];
+
+        for (i = 0; i < listOfTags.length; i++) {
+            var id = "tag_" + i;
+            console.log(document.getElementById(id).checked);
+            if (document.getElementById(id).checked) {
+                tags.push(listOfTags[i]);
+                document.getElementById(id).checked = false;
+            }
+        }
+
+        var result = connect.registerStudent(employeeId, studentId, fairId, tags);
+
+        if (result["registered"]) {
+            queue.shift();
+
+            var inner = "";
+            if (queue.length > 0) {
+                var student = queue[0];
+                inner +=
+                    "<div> Student: " +  
+                        "<div style=\"display: inline-block;\">" + student["name"] + "</div>" +             
+                        "<button style=\"display: inline-block;\" onclick=\"showStudentProfile('" + student["studentId"]+ "');\">Profile</button>" +
+                        "<button style=\"display: inline-block;\" onclick=\"skipStudent('" + student["studentId"]+ "');\">Skip</button>" +
+                        "<button style=\"display: inline-block;\" onclick=\"doneTalkingStudent('" + student["studentId"]+ "');\">Done Talking</button>" +
+                    "</div>";
+            }
+
+            for (i = 1; i < queue.length; i++) {
+                inner += 
+                    "<div> Student: " +  
+                        "<div style=\"display: inline-block;\">" + student["name"] + "</div>" +
+                    "</div>";
+            }
+
+            document.getElementById("queue").innerHTML = inner;
+        }
+
+        // if done talking with all students and the queue has been stopped, return to list_of_fairs page
+        if (queue.length == 0 && queueStopped) {
+            document.getElementById("fair_info_page").style.display = "block";
+            document.getElementById("queue_page").style.display = "none";
+            document.getElementById("stop_queue_desc").innerHTML = 
+                "<button id=\"stop_queue\">Stop My Queue</button>";
+        }
+
+        notOperatingQueue = true;
+    } else {
+        alert("Operation happened on current queue. Try again.");
+    }
+}
+
+// Edit profile
+document.getElementById("edit_employee_profile").onclick = function() {
+    document.getElementById("editing_profile").style.display = "block";
+    document.getElementById("employee_profile").style.display = "none";
+}
+// Submit profile changes
+document.getElementById("submit_employee_profile").onclick = function() {
+    document.getElementById("editing_profile").style.display = "none";
+    document.getElementById("employee_profile").style.display = "block";
+
+    var newName = document.getElementById("new_name").value;
+    var newEmail = document.getElementById("new_email").value;
+    var newBio = document.getElementById("new_bio").value;
+
+    if (newName === "") {
+        newName = employeeInfo["name"];
+    }
+    if (newEmail === "") {
+        newEmail = employeeInfo["email"];
+    }
+    if (newBio === "") {
+        newBio = employeeInfo["bio"];
+    }
+
+    var result = connect.updateEmployeeProfile(employeeInfo, newName, newEmail, newBio);
+    if (result["updated"]) {
+        employeeInfo["name"] = newName;
+        employeeInfo["email"] = newEmail;
+        employeeInfo["bio"] = newBio;
+
+        document.getElementById("employee_info").innerHTML = 
+            "<div>Image: " + employeeInfo["img"] + "</div>" +
+            "<div>Name: " + employeeInfo["name"] + "</div>" +
+            "<div>Company: " + employeeInfo["company"] + "</div>" +
+            "<div>Position: " + employeeInfo["position"] + "</div>" +
+            "<div>Email: " + employeeInfo["email"] + "</div>" +
+            "<div>Bio: " + employeeInfo["bio"] + "</div>";
+        
+        document.getElementById("new_name").value = "";
+        document.getElementById("new_email").value = "";
+        document.getElementById("new_bio").value = "";
+    }
+}
+
+
+
+// View career fairs history
+document.getElementById("view_history").onclick = function() {
+    document.getElementById("fairs_history_page").style.display = "block";
+    document.getElementById("employee_profile_page").style.display = "none";
+
+    listOfFairsHistory = connect.getPastFairsForEmployee(employeeId);
+    var innerListOfFairs = "";
+    for (i = 0; i < listOfFairsHistory.length; i++) {
+        var fairName = listOfFairsHistory[i]["name"];
+        innerListOfFairs +=
+            "<div>" +
+                "<div style=\"display: inline-block;\">" + fairName + " " + "</div>" +
+                "<button onclick=\"showFairInfoHistory('" + fairName + "');\">select</button>" +
+            "</div>";
+    }
+    document.getElementById("fairs_history").innerHTML = innerListOfFairs;
+}
+
+function showFairInfoHistory(fairName) {
+    document.getElementById("student_history_page").style.display = "block";
+    document.getElementById("fairs_history_page").style.display = "none";
+
+    var fair;
+    for (i = 0; i < listOfFairsHistory.length; i++) {
+        if (listOfFairsHistory[i]["name"] === fairName) {
+            fair = listOfFairsHistory[i];
+        }
+    }
+
+    var startTime = new Date(fair["start_time"]);
+    var endTime = new Date(fair["end_time"]);
+    document.getElementById("fair_history_info").innerHTML = 
+        "<div>" + fairName + "</div>" +
+        "<div>Location: " + fair["location"] + "</div>" +
+        "<div>Start Time: " + startTime + "</div>" +
+        "<div>End Time: " + endTime + "</div>" +
+        "<div>Description: " + fair["desc"] + "</div>";
+    var fairHistoryId = fair["fairId"];
+
+    // Get students for the fair
+    listOfStudentsHistory = connect.getPastStudentsForEmployee(employeeId, fairHistoryId);
+    var inner = "";
+    for (i = 0; i < listOfStudentsHistory.length; i++) {
+        var studentName = listOfStudentsHistory[i]["name"];
+        var studentId = listOfStudentsHistory[i]["studentId"];
+        var tags = listOfStudentsHistory[i]["tags"];
+
+        var stringTags = "";
+        if (tags.length > 0) {
+            for (j = 0; j < tags.length - 1; j++) {
+                stringTags += tags[j] + ", ";
+            }
+            stringTags += tags[tags.length - 1];
+        }
+
+        inner += 
+            "<div>" + 
+                "<div style=\"display: inline-block;\">" + studentName + "  Tags: " + stringTags + "</div>" +
+                "<button onclick=\"getStudentHistoryProfile('" + studentId + "'                         );\" style=\"display: inline-block;\">Profile</button>" +
+            "</div>";
+    }
+
+    document.getElementById("student_history").innerHTML = inner;
+}
+
+// View one student history
+function getStudentHistoryProfile(studentId) {
+    document.getElementById("student_history_profile_page").style.display = "block";
+    document.getElementById("student_history_page").style.display = "none";
+
+    var student = connect.getStudentProfile(studentId);
+    document.getElementById("student_history_info").innerHTML = 
+        "<div> Image: " + student["img"] + "</div>" +
+        "<div> Name: " + student["name"] + "</div>" +
+        "<div> University: " + student["university"] + "</div>" +
+        "<div> Major: " + student["major"] + "</div>" +
+        "<div> Graduation Date: " + new Date(student["graduationDate"]) + "</div>" +
+        "<div> Resume: " + student["resume"] + "</div>";
+}
+
+
 
 // Back to login page
 document.getElementById("back_to_login").onclick = function() {
@@ -95,69 +388,33 @@ document.getElementById("back_to_fairs_e").onclick = function() {
 
 // Back to fair_info page
 document.getElementById("stop_queue").onclick = function() {
-    document.getElementById("fair_info_page").style.display = "block";
-    document.getElementById("queue_page").style.display = "none";
+    var result = connect.stopMyQueue(employeeId);
+    if (result["stopped"]) {
+        clearInterval(update);
+        queueStopped = true;
+        document.getElementById("stop_queue_desc").innerHTML = 
+            "<div>Your queue has been stopped. No more students will join your queue. Returning to list of fairs page after done talking with current students in the queue.</div>";
+    }
 }
 
 // Back to queue info page
 document.getElementById("back_to_queue").onclick = function() {
     document.getElementById("queue_page").style.display = "block";
     document.getElementById("student_profile_page").style.display = "none";
-    // TODO: Save tags
 }
 
-
-// Edit profile
-document.getElementById("edit_employee_profile").onclick = function() {
-    document.getElementById("editing_profile").style.display = "block";
-    document.getElementById("employee_profile").style.display = "none";
-}
-// Submit profile changes
-document.getElementById("submit_employee_profile").onclick = function() {
-    document.getElementById("editing_profile").style.display = "none";
-    document.getElementById("employee_profile").style.display = "block";
-}
-
-
-
-// View career fairs history
-document.getElementById("view_history").onclick = function() {
-    document.getElementById("fairs_history_page").style.display = "block";
-    document.getElementById("employee_profile_page").style.display = "none";
-}
 // Back to employee_profile
 document.getElementById("back_to_profile").onclick = function() {
     document.getElementById("fairs_history_page").style.display = "none";
     document.getElementById("employee_profile_page").style.display = "block";
 }
-// View one career fair history
-document.getElementById("select_fair_history").onclick = function() {
-    document.getElementById("student_history_page").style.display = "block";
-    document.getElementById("fairs_history_page").style.display = "none";
-}
-// Back to employee_profile
+// Back to fairs_history_page
 document.getElementById("back_to_fair_history").onclick = function() {
     document.getElementById("student_history_page").style.display = "none";
     document.getElementById("fairs_history_page").style.display = "block";
 }
-// View one student history
-document.getElementById("student_history_profile").onclick = function() {
-    document.getElementById("student_history_profile_page").style.display = "block";
-    document.getElementById("student_history_page").style.display = "none";
-}
-// Back to employee_profile
+// Back to student_history_page
 document.getElementById("back_to_student_history").onclick = function() {
     document.getElementById("student_history_profile_page").style.display = "none";
     document.getElementById("student_history_page").style.display = "block";
-}
-
-// Remove student
-document.getElementById("skip_student_1").onclick = function() {
-    document.getElementById("student_1").style.display = "none";
-}
-
-// Remove student
-document.getElementById("remove_student_1").onclick = function() {
-    document.getElementById("student_1").style.display = "none";
-    // TODO: Save tags
 }
